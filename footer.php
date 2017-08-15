8b87acc01bd51fabed24bb92cc2b721b9f634e33
Sam <cawoodsa@msu.edu>
2017-08-15 19:45:21 -0400

    Phone numba

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <h2>Cawood Building Co</h2>
     <div>
         <h3>Contact Us</h3>
-        <p><i class="material-icons">phone</i> 517-XXX-XXXX</p>
+        <p><i class="material-icons">phone</i> 517-349-9450</p>
         <p><i class="material-icons">email</i> email@example.com</p>
     </div>
     <p>Hey</p>
