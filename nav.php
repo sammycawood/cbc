3ddf1b02f9331848f1bf45baba1a78b9b5727c61
Mikelsavage <n.mikelsavage@gmail.com>
2017-08-16 16:58:50 -0400

    Navigation links working

--- nav.php
+++ nav.php
@@ -2,9 +2,9 @@
     <nav id="mainNav">
         <h1 class="logo left"><a href="index.php"><img src="graphics/CBCLogo.png" alt="Cawood Building Co."></a></h1>
         <ul class="right">
-            <a href="index.php"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
-            <a href="index.php/#ourStory"><li>Our Story</li></a>
-            <a href="index.php/#buildingProcess"><li>Building Process</li></a>
+            <a href="http://cawoodbuildingco.com/"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
+            <a href="http://cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
+            <a href="http://cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
             <a href="gallery.php"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
             <a href="properties-rentals.php"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
         </ul>
@@ -14,9 +14,9 @@
 <nav class="nav" id="mobileNav">
     <div id="mobileNavContainer">
         <ul>
-            <a href="cawoodbuildingco.com/"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
-            <a href="cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
-            <a href="cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
+            <a href="http://cawoodbuildingco.com/"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
+            <a href="http://cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
+            <a href="http://cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
             <a href="gallery.php"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
             <a href="properties-rentals.php"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
         </ul>
