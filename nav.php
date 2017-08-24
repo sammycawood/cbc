b4933980bc60e7624c2d4310072c15e9d0479336
Mikelsavage <n.mikelsavage@gmail.com>
2017-08-24 14:13:43 -0400

    update navigation links

--- nav.php
+++ nav.php
@@ -1,12 +1,23 @@
+<?php
+    define('Site_Url', 'http://cawoodbuildingco.com/');
+    $links = [
+        Site_Url . '#top',
+        Site_Url . '#ourStory',
+        Site_Url . '#buildingProcess',
+        Site_Url . 'gallery',
+        Site_Url . 'properties-rentals'
+    ]
+?>
+
 <header>
     <nav id="mainNav">
         <h1 class="logo left"><a href="http://cawoodbuildingco.com/"><img src="graphics/CBCLogo.png" alt="Cawood Building Co."></a></h1>
         <ul class="right">
-            <a href="http://cawoodbuildingco.com/#top"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
-            <a href="http://cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
-            <a href="http://cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
-            <a href="gallery.php"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
-            <a href="properties-rentals.php"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
+            <a href="<?php echo $links[0]; ?>"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
+            <a href="<?php echo $links[1]; ?>"><li>Our Story</li></a>
+            <a href="<?php echo $links[2]; ?>"><li>Building Process</li></a>
+            <a href="<?php echo $links[3]; ?>"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
+            <a href="<?php echo $links[4]; ?>"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
         </ul>
         <p class="mobileIconContainer right"><a href="javascript:void(0);" id="mobileIcon">&#9776;</a></p>
     </nav>
@@ -14,11 +25,11 @@
 <nav class="nav" id="mobileNav">
     <div id="mobileNavContainer">
         <ul>
-            <a href="http://cawoodbuildingco.com/"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
-            <a href="http://cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
-            <a href="http://cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
-            <a href="gallery.php"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
-            <a href="properties-rentals.php"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
+            <a href="<?php echo $links[0]; ?>"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
+            <a href="<?php echo $links[1]; ?>"><li>Our Story</li></a>
+            <a href="<?php echo $links[2]; ?>"><li>Building Process</li></a>
+            <a href="<?php echo $links[3]; ?>"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
+            <a href="<?php echo $links[4]; ?>"><li <?php if ($page_title == 'Properties & Rentals') echo "class='active'"; ?>>Properties &amp; Rentals</li></a>
         </ul>
     </div>
 </nav>
