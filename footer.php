da73d4b9bcf820ad31bd5a050892df291a5db129
Mikelsavage <n.mikelsavage@gmail.com>
2017-08-24 13:48:06 -0400

    Footer spacing and mws credit

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 <footer>
-    <h2>Cawood Building Co</h2>
-    <div>
-        <h3>Contact Us</h3>
-        <p><i class="material-icons">phone</i> 517-349-9450</p>
-        <p><i class="material-icons">email</i> email@example.com</p>
+    <div class="flexWrap">
+        <h2>Cawood Building Co</h2>
+        <div>
+            <h3>Contact Us</h3>
+            <p><i class="material-icons">phone</i> 517-349-9450</p>
+            <p><i class="material-icons">email</i> email@example.com</p>
+        </div>
+    </div>
+    <div class="mwsCredit">
+        <p><span class="smallText">Design &amp; Development by </span>Marble Web Solutions</p>
     </div>
 </footer>
--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <header>
     <nav id="mainNav">
-        <h1 class="logo left"><a href="index.php"><img src="graphics/CBCLogo.png" alt="Cawood Building Co."></a></h1>
+        <h1 class="logo left"><a href="http://cawoodbuildingco.com/"><img src="graphics/CBCLogo.png" alt="Cawood Building Co."></a></h1>
         <ul class="right">
-            <a href="http://cawoodbuildingco.com/"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
+            <a href="http://cawoodbuildingco.com/#top"><li <?php if ($page_title == 'Home') echo "class='active'"; ?>>Home</li></a>
             <a href="http://cawoodbuildingco.com/#ourStory"><li>Our Story</li></a>
             <a href="http://cawoodbuildingco.com/#buildingProcess"><li>Building Process</li></a>
             <a href="gallery.php"><li <?php if ($page_title == 'Gallery') echo "class='active'"; ?>>Gallery</li></a>
