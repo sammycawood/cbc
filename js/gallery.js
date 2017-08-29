f801bc2999e052e12ba1fd522a00d4b22be35cfc
Mikelsavage <n.mikelsavage@gmail.com>
2017-08-29 15:27:40 -0400

    gallery slider functioning

--- js/gallery.js
+++ js/gallery.js
@@ -15,12 +15,18 @@ $(document).ready(function() {
     // When the user clicks on the button, open the modal
     $(pics).click(function() {
         that.modalIndex = parseInt($(this)[0].id);
+        var ndx = that.modalIndex;
+        console.log(ndx);
+        $(modal).fadeIn();
 
         // Image slider
         var mySwiper = new Swiper ('.swiper-container', {
             // Optional parameters
+            autoHeight: true,
+            effect: "coverflow",
+            initialSlide: ndx,
+            keyboardControl: true,
             loop: true,
-            initialSlide: that.modalIndex,
 
             // If we need pagination
             // pagination: '.swiper-pagination',
@@ -30,7 +36,7 @@ $(document).ready(function() {
             prevButton: '.swiper-button-prev'
         });
 
-        $(modal).fadeIn();
+
     });
 
     // When the user clicks on 'x', close the modal
@@ -45,28 +51,6 @@ $(document).ready(function() {
         }
     });
 
-    // Clicking arrows when modal is open
-    $("#leftArrow").click(function() {
-        that.modalIndex--;
-        if (that.modalIndex < 0) {
-            that.modalIndex = pics.length - 1;
-        }
-        updateModal(that.modalIndex);
-    });
-
-    $("#rightArrow").click(function() {
-        that.modalIndex++;
-        if (that.modalIndex >= pics.length) {
-            that.modalIndex = 0;
-        }
-        updateModal(that.modalIndex);
-    });
-
-    // Put in correct image into modal by index
-    function updateModal(index) {
-        $($(modal).find("img").get(0)).attr("src", $($(pics)[index]).find("img")[0].src);
-    }
-
     // Make pictures load every 0.5s
     var index = 0;
     var galInt = setInterval(function() {
@@ -76,29 +60,4 @@ $(document).ready(function() {
             clearInterval(galInt);
         }
     }, 500);
-
-    // Modal fits window at any size, make sure it's not too big
-    function checkModalSize() {
-        // Ignore this function for small devices
-        if ($(window).width() < 768) {
-            return;
-        }
-
-        var winHeight = parseFloat($(window).height() - $('header').height()); // Exclude header
-        var winModWidth = parseFloat($(window).width());
-        var modHeight = parseFloat($('.modal-content').height());
-        var modWidth = parseFloat($('.modal-content').width());
-
-
-        /*
-        if () {
-            $('.modal-content').css('width', 'auto');
-            $('.modal-content').css('height', '');
-        } else {
-            $('.modal-content').css('width', '80%');
-            $('.modal-content').css('height', 'auto');
-        }
-        */
-    }
-    $(window).resize(checkModalSize);
 });
