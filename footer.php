e4941f2f221b4294d22c4731b950eaf55de6426f
Mikelsavage <n.mikelsavage@gmail.com>
2017-09-05 13:23:18 -0400

    Footer and home page numbers background update

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <footer>
     <div class="flexWrap">
-        <h2>Cawood Building Co</h2>
+        <div>
+            <h3>Learn More</h3>
+            <a href="<?php echo $links[0] ?>"><p>Top</p></a>
+        </div>
         <div>
             <h3>Contact Us</h3>
-            <p><i class="material-icons">phone</i> 517-349-9450</p>
-            <a href="mailto:CawoodBuilding@att.net"><p><i class="material-icons">email</i> CawoodBuilding@att.net</p></a>
+            <p><a href="tel:517-349-9450"><i class="material-icons">phone</i> 517-349-9450</p>
+            <p><a href="mailto:CawoodBuilding@att.net"><i class="material-icons">email</i> CawoodBuilding@att.net</a></p>
         </div>
     </div>
     <div class="mwsCredit">
